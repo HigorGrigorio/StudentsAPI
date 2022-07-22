63374bd91e0e8cbc173e7a3857c1c8c2bdffd791
Higor Grigorio <higorgrigorio@gmail.com>
2022-07-22 16:43:46 -0300

    update cors

--- dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
-var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
-
-// load .env configs
-_dotenv2.default.config();
-
-const { PORT } = process.env;
-
-_app2.default.listen(PORT);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Express from 'express';
 import dotenv from 'dotenv';
 import { resolve } from 'path';
+import Cors from 'cors';
+import Helmet from 'helmet';
 import StudentRouter from './routes/StudentRouter';
 import UserRouter from './routes/UserRouter';
 import TokenRouter from './routes/TokenRouter';
@@ -10,6 +12,21 @@ import './database';
 
 // load .env configs
 dotenv.config();
+
+const whiteList = [
+  'http://34.151.233.122',
+  'http://localhost:3000',
+];
+
+const corsOption = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App extends Express {
   constructor() {
@@ -22,6 +39,12 @@ class App extends Express {
     // express middlewares configuration
     this.use(Express.urlencoded({ extended: true }));
     this.use(Express.json());
+
+    /**
+     * configure CORS
+     */
+    this.use(Cors(corsOption));
+    this.use(Helmet());
 
     // configure a static path
     this.use('/images', Express.static(resolve(__dirname, 'uploads', 'images')));
